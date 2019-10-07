d0ac4dfd8b62ba04a5f239f114ab07f8f60d351b
Nekhil Nagia <nagianek@Nekhils-MacBook-Pro.local>
2019-10-07 15:16:06 -0400

    better formatting

--- set1/problem4-experiment.py
+++ set1/problem4-experiment.py
@@ -3,15 +3,17 @@ def main():
 
     with open('problem4.txt') as f:
         content = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
     content = [x.strip() for x in content]
+
     highest_score = -1
     best_result = ''
+
     for c in content:
         result = single_char_key_search(c)
         if result[1] > highest_score:
             highest_score = result[1]
             best_result = result[0]
+
     print(best_result.decode())
 if __name__ == '__main__':
     main()
